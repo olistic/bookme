a0ea6ff81d44493bf3a60e1cd7109c019e46961b
Matias Olivera <moliverafreire@gmail.com>
2019-10-02 21:56:15 -0300

    Don't register connectors plugin for tests

--- api/lib/server.js
+++ api/lib/server.js
@@ -2,21 +2,22 @@
 
 const Hapi = require('@hapi/hapi');
 
+const connectors = require('./connectors');
+const routes = require('./routes');
+
 const server = Hapi.server({
   port: 3000,
   host: 'localhost',
 });
 
-const plugins = [require('./connectors'), require('./routes')];
-
 exports.init = async () => {
-  await server.register(plugins);
+  await server.register([routes]);
   await server.initialize();
   return server;
 };
 
 exports.start = async () => {
-  await server.register(plugins);
+  await server.register([connectors, routes]);
   await server.start();
   console.log(`Server running at: ${server.info.uri}`); // eslint-disable-line no-console
   return server;
