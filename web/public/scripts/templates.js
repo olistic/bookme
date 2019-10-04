e7bab65cdf822f8f98494bbc558de0d8799a22e2
Matias Olivera <moliverafreire@gmail.com>
2019-10-04 15:57:57 -0300

    Add home template

--- web/public/scripts/templates.js
+++ web/public/scripts/templates.js
@@ -1,3 +1,4 @@
+export { default as home } from './templates/home.js';
 export { default as login } from './templates/login.js';
 export { default as notFound } from './templates/notFound.js';
 export { default as signup } from './templates/signup.js';
