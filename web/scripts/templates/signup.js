bd810721d04189934bfe67dc039822102ba39a49
Matias Olivera <moliverafreire@gmail.com>
2019-10-04 18:59:59 -0300

    Improve forms (drive-by)

--- web/scripts/templates/signup.js
+++ web/scripts/templates/signup.js
@@ -4,8 +4,22 @@ const signup = () => html`
   <div>
     <h1>Sign up for bookme</h1>
     <form>
-      <input type="email" />
-      <input type="password" />
+      <label>
+        Email:
+        <input type="email" />
+      </label>
+      <label>
+        Password:
+        <input type="password" />
+      </label>
+      <label>
+        First name:
+        <input type="text" />
+      </label>
+      <label>
+        Last name:
+        <input type="text" />
+      </label>
       <button type="submit">Sign up</button>
       <p>Already have an account? <a href="/login">Log in</a></p>
     </form>
