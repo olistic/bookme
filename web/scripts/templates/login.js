ce5216dd5038875a36f5c58bbd33cfabdda79aba
Matias Olivera <moliverafreire@gmail.com>
2019-10-05 00:27:01 -0300

    Connect login page to api

--- web/scripts/templates/login.js
+++ web/scripts/templates/login.js
@@ -1,21 +1,34 @@
 import { html } from '../../node_modules/lit-html/lit-html.js';
 
-const login = () => html`
-  <div>
-    <h1>Log in to bookme</h1>
-    <form>
-      <label>
-        Email:
-        <input type="email" />
-      </label>
-      <label>
-        Password:
-        <input type="password" />
-      </label>
-      <button type="submit">Log in</button>
-      <p>Don't have an account? <a href="/signup">Sign up</a></p>
-    </form>
-  </div>
-`;
+import { logIn } from '../services/auth.js';
+
+const login = () => {
+  const handleSubmit = async event => {
+    event.preventDefault();
+
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+
+    await logIn(email, password);
+  };
+
+  return html`
+    <div>
+      <h1>Log in to bookme</h1>
+      <form @submit=${handleSubmit}>
+        <label>
+          Email:
+          <input name="email" type="email" />
+        </label>
+        <label>
+          Password:
+          <input name="password" type="password" />
+        </label>
+        <button>Log in</button>
+        <p>Don't have an account? <a href="/signup">Sign up</a></p>
+      </form>
+    </div>
+  `;
+};
 
 export default login;
