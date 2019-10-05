426934a6a9fccb2a2e8819f783ec31fa4f29db09
Matias Olivera <moliverafreire@gmail.com>
2019-10-05 00:26:34 -0300

    Add auth service

---
+++ web/scripts/services/auth.js
@@ -0,0 +1,36 @@
+import { get, post } from '../utils/api.js';
+import { removeSession, setSession } from '../utils/session.js';
+
+const baseUrl = 'http://localhost:3000';
+
+export const logIn = async (email, password) => {
+  const { data: session, error } = await post(`${baseUrl}/sessions`, {
+    email,
+    password,
+  });
+  if (error) {
+    if (error.status === 401) {
+      throw new Error('Invalid email/password combination.');
+    }
+
+    throw new Error('Oops! Something went wrong...');
+  }
+
+  setSession(session);
+  window.location.assign('/');
+};
+
+export const logOut = () => {
+  removeSession();
+  window.location.assign('/login');
+};
+
+export const getLoggedInUser = async () => {
+  const { data: user } = await get(`${baseUrl}/me`);
+  if (!user) {
+    logOut();
+    return null;
+  }
+
+  return user;
+};
