788f6d780f40c1781cc6f164d918eb6ae646d47b
Matias Olivera <moliverafreire@gmail.com>
2019-10-05 00:28:17 -0300

    Add logout button

--- web/scripts/templates/home.js
+++ web/scripts/templates/home.js
@@ -1,8 +1,11 @@
 import { html } from '../../node_modules/lit-html/lit-html.js';
 
+import { logOut } from '../services/auth.js';
+
 const home = () => html`
   <div>
     <h1>Welcome to bookme</h1>
+    <button @click=${logOut} type="button">Log out</button>
   </div>
 `;
 
