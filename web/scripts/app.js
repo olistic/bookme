2058da72a68db49a66867e712c28c2fe0bb38d43
Matias Olivera <moliverafreire@gmail.com>
2019-10-05 00:34:26 -0300

    Separate authenticated and unauthenticated pages

--- web/scripts/app.js
+++ web/scripts/app.js
@@ -1,14 +1,26 @@
 import { render } from '../node_modules/lit-html/lit-html.js';
 
+import { getLoggedInUser } from './services/auth.js';
+import { getSession } from './utils/session.js';
 import { home, login, notFound, signup } from './templates.js';
 
-const routes = {
+const authenticatedRoutes = {
   '/': home,
+};
+const unauthenticatedRoutes = {
   '/login': login,
   '/signup': signup,
 };
 const defaultRoute = notFound;
 
-const template = routes[window.location.pathname] || defaultRoute;
-const rootEl = document.getElementById('root');
-render(template(), rootEl);
+const app = async () => {
+  const session = getSession();
+  const user = session ? await getLoggedInUser() : null;
+  const isAuthenticated = !!user;
+  const routes = isAuthenticated ? authenticatedRoutes : unauthenticatedRoutes;
+  const template = routes[window.location.pathname] || defaultRoute;
+  const rootEl = document.getElementById('root');
+  render(template(), rootEl);
+};
+
+app();
