033d25ed65381f751c661a5bd2eedb1a8c5f7264
Matias Olivera <moliverafreire@gmail.com>
2019-10-23 18:02:13 -0300

    Style login page

--- web/scripts/templates/login.js
+++ web/scripts/templates/login.js
@@ -1,34 +1,29 @@
 import { html } from '../../node_modules/lit-html/lit-html.js';
+import { styleMap } from '../../node_modules/lit-html/directives/style-map.js';
 
-import { logIn } from '../services/auth.js';
+import loginForm from './loginForm.js';
 
-const login = () => {
-  const handleSubmit = async event => {
-    event.preventDefault();
-
-    const email = event.target.email.value;
-    const password = event.target.password.value;
-
-    await logIn(email, password);
-  };
-
-  return html`
-    <div>
-      <h1>Log in to bookme</h1>
-      <form @submit=${handleSubmit}>
-        <label>
-          Email:
-          <input name="email" type="email" />
-        </label>
-        <label>
-          Password:
-          <input name="password" type="password" />
-        </label>
-        <button>Log in</button>
-        <p>Don't have an account? <a href="/signup">Sign up</a></p>
-      </form>
+const login = () => html`
+  <main style=${styleMap(styles.main)}>
+    <div style=${styleMap(styles.container)}>
+      ${loginForm()}
     </div>
-  `;
+  </main>
+`;
+
+const styles = {
+  main: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh',
+    paddingTop: '40px',
+  },
+  container: {
+    alignItems: 'center',
+    display: 'flex',
+    flex: 1,
+    justifyContent: 'center',
+  },
 };
 
 export default login;
---
+++ web/scripts/templates/loginForm.js
@@ -0,0 +1,56 @@
+import { html } from '../../node_modules/lit-html/lit-html.js';
+import { styleMap } from '../../node_modules/lit-html/directives/style-map.js';
+
+import { logIn } from '../services/auth.js';
+
+const loginForm = () => {
+  const handleSubmit = async event => {
+    event.preventDefault();
+
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+
+    await logIn(email, password);
+  };
+
+  return html`
+    <form @submit=${handleSubmit} style=${styleMap(styles.form)}>
+      <h1 style=${styleMap(styles.heading)}>Log in to <strong>bookme</strong></h1>
+      <div class="form-group">
+        <label>
+          <span>Email</span>
+          <input name="email" placeholder="email@domain.com" type="email" />
+        </label>
+      </div>
+      <div class="form-group last">
+        <label>
+          <span>Password</span>
+          <input name="password" placeholder="********" type="password" />
+        </label>
+      </div>
+      <button class="primary large">Log in</button>
+    </form>
+  `;
+};
+
+const styles = {
+  form: {
+    backgroundColor: '#ffffff',
+    borderRadius: '6px',
+    boxShadow: '',
+    display: 'flex',
+    flexDirection: 'column',
+    maxWidth: '100%',
+    padding: '64px 96px',
+    width: '544px',
+  },
+  heading: {
+    fontSize: '26px',
+    fontWeight: 400,
+    lineHeight: 1.23,
+    margin: '0 0 24px',
+    textAlign: 'center',
+  },
+};
+
+export default loginForm;
