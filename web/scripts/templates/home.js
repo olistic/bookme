4530b304d9057df4972d0484bbf78c5248839d4e
Matias Olivera <moliverafreire@gmail.com>
2019-11-09 19:03:50 -0300

    Style home page

--- web/scripts/templates/home.js
+++ web/scripts/templates/home.js
@@ -1,12 +1,22 @@
 import { html } from '../../node_modules/lit-html/lit-html.js';
+import { styleMap } from '../../node_modules/lit-html/directives/style-map.js';
 
 import { logOut } from '../services/auth.js';
 
 const home = () => html`
   <div>
-    <h1>Welcome to bookme</h1>
-    <button @click=${logOut} type="button">Log out</button>
+    <h1 style=${styleMap(styles.heading)}>Welcome to <strong>bookme</strong></h1>
+    <button @click=${logOut} class="secondary medium" type="button">Log out</button>
   </div>
 `;
+
+const styles = {
+  heading: {
+    fontSize: '26px',
+    fontWeight: 400,
+    lineHeight: 1.23,
+    margin: '0 0 24px',
+  },
+};
 
 export default home;
