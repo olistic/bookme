e54b264bd55434a52ab309d944cded121637e61b
Matias Olivera <moliverafreire@gmail.com>
2019-11-11 16:48:05 -0300

    Use custom router in authed and unauthed apps

--- web/scripts/app.js
+++ web/scripts/app.js
@@ -1,16 +1,17 @@
-import { render } from '../node_modules/lit-html/lit-html.js';
-
 import authenticatedApp from './authenticatedApp.js';
 import unauthenticatedApp from './unauthenticatedApp.js';
 import { getLoggedInUser } from './services/auth.js';
 import { getSession } from './utils/session.js';
 
-const app = async () => {
+const renderApp = async () => {
   const session = getSession();
   const user = session ? await getLoggedInUser() : null;
   const isAuthenticated = !!user;
-  const rootEl = document.getElementById('root');
-  render(isAuthenticated ? authenticatedApp() : unauthenticatedApp(), rootEl);
+  if (isAuthenticated) {
+    authenticatedApp();
+  } else {
+    unauthenticatedApp();
+  }
 };
 
-app();
+renderApp();
