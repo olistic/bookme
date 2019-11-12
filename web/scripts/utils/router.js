4afb2faa623a4f8ca57830a9a80a959a59bdab67
Matias Olivera <moliverafreire@gmail.com>
2019-11-12 10:40:03 -0300

    Add advanced path matching logic

--- web/scripts/utils/router.js
+++ web/scripts/utils/router.js
@@ -1,3 +1,4 @@
+import { pathToRegexp } from '../../node_modules/path-to-regexp/dist.es2015/index.js';
 import { render } from '../../node_modules/lit-html/lit-html.js';
 
 /**
@@ -16,18 +17,65 @@ export const navigate = path => {
   window.dispatchEvent(pushstateEvent);
 };
 
+/**
+ * Match a URL pathname to a path.
+ *
+ * See:
+ * https://github.com/ReactTraining/react-router/blob/ef60b08d514e1db3354316c303783158f108bd3b/packages/react-router/modules/matchPath.js.
+ *
+ * @param {string} pathname The URL pathname.
+ * @param {string} path The path.
+ * @param {boolean} exact Whether the match should be exact or not.
+ *
+ * @returns {Object} The match object if there was a match, null otherwise.
+ */
+const matchPath = (pathname, path, exact = false) => {
+  const keys = [];
+  const regexp = pathToRegexp(path, keys, {
+    end: exact,
+  });
+
+  const match = regexp.exec(pathname);
+
+  if (!match) {
+    return null;
+  }
+
+  const [url, ...values] = match;
+
+  const isExact = pathname === url;
+  if (exact && !isExact) {
+    return null;
+  }
+
+  return {
+    path,
+    url: path === '/' && url === '' ? '/' : url,
+    isExact,
+    params: keys.reduce(
+      (params, key, index) => ({
+        ...params,
+        [key.name]: values[index],
+      }),
+      {},
+    ),
+  };
+};
+
 export class Route {
-  constructor(path, template) {
+  constructor(path, template, exact = false) {
     this.path = path;
     this.template = template;
+    this.exact = exact;
   }
 
   match() {
-    return this.path === window.location.pathname;
+    return matchPath(window.location.pathname, this.path, this.exact);
   }
 
   render() {
-    return this.match() ? this.template() : null;
+    const match = this.match();
+    return match ? this.template(match) : null;
   }
 }
 
